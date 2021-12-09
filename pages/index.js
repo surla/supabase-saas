2b3c394c6030c610107603c5f9a7892463cad76a
Joseph Surla <josephsurla@gmail.com>
2021-12-08 18:36:22 -0800

    query single record from supabase

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
 import { supabase } from "../utils/supabase";
+import Link from "next/link";
 
-export default function Home() {
+export default function Home({ lessons }) {
+  console.log({ lessons });
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      working
+    <div className="w-full max-w-3xl px-2 mx-auto my-16">
+      {lessons.map((lesson) => (
+        <Link key={lesson.id} href={`/${lesson.id}`}>
+          <a className="flex h-40 p-8 mb-4 text-xl rounded shadow">
+            {lesson.title}
+          </a>
+        </Link>
+      ))}
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const { data: lessons } = await supabase.from("lesson").select("*");
+
+  return {
+    props: {
+      lessons,
+    },
+  };
+};
